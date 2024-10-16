06455d479bcd283ecd8fee8c51250869b1791745
Jaume Parrot Altisent <jparrot.plexus@externos.avatel.es>
2024-10-16 10:37:57 +0200

    Juego tic tac toe

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,34 +5,54 @@ import { Square } from "./components/Square";
 import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js";
 
 function App() {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [turn, setTurn] = useState(TURNS.X);
+    const [board, setBoard] = useState(() => {
+        const boardFromStorage = window.localStorage.getItem("board");
+        if (boardFromStorage) return JSON.parse(boardFromStorage);
+        return Array(9).fill(null);
+    });
+
+    const [turn, setTurn] = useState(() => {
+        const turnFromStorage = window.localStorage.getItem("turn");
+        return turnFromStorage ?? TURNS.X;
+    });
+
+    // null es que no hay ganador, false es que hay un empate
     const [winner, setWinner] = useState(null);
 
     const resetGame = () => {
         setBoard(Array(9).fill(null));
         setTurn(TURNS.X);
         setWinner(null);
+
+        resetGameStorage();
     };
 
     const updateBoard = (index) => {
+        // no actualizamos esta posición
+        // si ya tiene algo
         if (board[index] || winner) return;
-
+        // actualizar el tablero
         const newBoard = [...board];
         newBoard[index] = turn;
         setBoard(newBoard);
-
+        // cambiar el turno
         const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
         setTurn(newTurn);
-
+        // guardar aqui partida
+        saveGameToStorage({
+            board: newBoard,
+            turn: newTurn,
+        });
+        // revisar si hay ganador
         const newWinner = checkWinnerFrom(newBoard);
         if (newWinner) {
             confetti();
             setWinner(newWinner);
         } else if (checkEndGame(newBoard)) {
-            setWinner(false);
+            setWinner(false); // empate
         }
     };
 
